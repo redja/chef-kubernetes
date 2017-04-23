d216a3aa49ddd706f97708a6616148c5ba3d3527
Maxim Filatov <pipopolam@gmail.com>
2017-04-23 14:22:25 +0300

    Preserve cluster dns systemwide

--- recipes/networking.rb
+++ recipes/networking.rb
@@ -5,7 +5,18 @@
 # Author:: Maxim Filatov <bregor@evilmartians.com>
 #
 
+require 'resolv'
+
 include_recipe 'network_interfaces'
+
+if node['kubernetes']['use_cluster_dns_systemwide']
+  nameservers = Resolv::DNS::Config.new.lazy_initialize.nameserver_port.map(&:first)
+  nameservers = Array(node['kubernetes']['cluster_dns']) + nameservers
+  nameservers.uniq!
+  network_interfaces 'lo' do
+    custom "dns-nameservers" => nameservers.join(' ')
+  end
+end
 
 network_interfaces node['kubernetes']['interface'] do
   post_up "/sbin/ip route replace #{node['kubernetes']['api']['service_cluster_ip_range']} dev #{node['kubernetes']['interface']}"
