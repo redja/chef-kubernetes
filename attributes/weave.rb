7b8d8db4bd7178ce6485bead6534fd748201ade4
Maxim Filatov <pipopolam@gmail.com>
2017-08-21 16:23:28 +0300

    Weave: portmap plugin conflist

--- attributes/weave.rb
+++ attributes/weave.rb
@@ -2,5 +2,6 @@ default['kubernetes']['weave']['version']                = '2.0.1'
 default['kubernetes']['weave']['network']                = '192.168.0.0/16'
 default['kubernetes']['weave']['interface']              = 'eth1'
 default['kubernetes']['weave']['use_scope']              = true
+default['kubernetes']['weave']['use_portmap']            = false
 default['kubernetes']['weavescope']['version']           = '0.17.1'
 default['kubernetes']['weavescope']['port']              = '4040'
